5cae3c1b516c058bc39d3547c3c893bfd94d1d69
Jchamo <joshchamo@gmail.com>
2021-02-25 12:04:27 -0500

    Fixed routes and added Join/Chat scripts

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import Join from './components/Join';
+import Chat from './components/Chat'
+
+/* User is greeted with Join component at route, user's login data will pass through query string to Chat component which will then render Chat */
+const App = () => (
+  <Router>
+    <Route path="/" exact component={Join} />
+    <Route path="/chat" component={Chat} />
+  </Router>
+);
+
+export default App;
